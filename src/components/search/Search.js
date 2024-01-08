f027c49659474824a9f137c8ee3bba30f0fcc58b
Riad <riad-esrefov@mail.ru>
2024-01-08 22:16:53 +0200

    Search validation

--- src/components/search/Search.js
+++ src/components/search/Search.js
@@ -1,38 +1,43 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import { useTranslation } from "react-i18next";
 
 export default function Search({ searchCity }) {
   const [value, setValue] = useState("");
+  const [validated, setValidated] = useState("");
   const { t } = useTranslation();
 
   function handleSubmit(e) {
     e.preventDefault();
+    console.log(validated);
+    console.log(value.length);
     if (value) {
       searchCity(value);
     }
-    console.log(!e.currentTarget.checkValidity());
-    if (!e.currentTarget.checkValidity()) {
-      e.preventDefault();
-      e.stopPropagation();
-    }
+    value.length === 0 ? setValidated("was-validated") : setValidated("");
   }
+
+  useEffect(()=>{
+    if (value) {
+      value.length === 0 ? setValidated("was-validated") : setValidated("");      
+    }
+    console.log(value);
+    console.log(value.length);
+  },[value])
 
   return (
     <form
-      className="d-flex needs-validation"
-      role="search"
+      className={`d-flex needs-validation ${validated}`}
       onSubmit={handleSubmit}
+      noValidate
     >
       <input
         className="form-control me-2"
         type="text"
         placeholder={t("header.search")}
-        aria-label="Search"
         value={value}
-        onChange={(e) => setValue(e.target.value)}
-        // required
+        onInput={e=> {setValue(e.target.value);}}
+        required
       />
-      <div className="valid-feedback">Wrong city</div>
       <button className="btn btn-outline-success" type="submit">
         {t("header.search")}
       </button>
