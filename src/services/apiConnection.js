63ac35454474e37c1b43020bdd8f3ffa61720b06
Slater <riad8242@gmail.com>
2024-01-20 00:46:21 +0000

    context added

--- src/services/apiConnection.js
+++ src/services/apiConnection.js
@@ -3,13 +3,17 @@ const apiKey = "64555bc1e6d8ac3073853234e29a5595";
 
 export default async function getWeather(city, lang) {
   const cityRes = await fetch(
-    `${apiBase}geo/1.0/direct?q=${city}&appid=${apiKey}`
+    `${apiBase}geo/1.0/direct?q=${city}&appid=${apiKey}`,
   );
-  const cityData = await cityRes.json();
-  const weatherRes = await fetch(
-    `${apiBase}data/2.5/weather?lat=${cityData?.[0]?.lat}&lon=${cityData?.[0]?.lon}&appid=${apiKey}&units=metric&lang=${lang}`
-  );
-  const weatherData = await weatherRes.json();
+  const cityArr = await cityRes.json();
 
-  return [weatherData, ...cityData];
+  if (cityArr.cod != "400") {
+    const [cityData] = cityArr;
+    const weatherRes = await fetch(
+      `${apiBase}data/2.5/weather?lat=${cityData.lat}&lon=${cityData.lon}&appid=${apiKey}&units=metric&lang=${lang}`,
+    );
+    const weatherData = await weatherRes.json();
+
+    return [weatherData, ...cityArr];
+  }
 }
